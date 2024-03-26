39f1557f74b849cb55b06bfca92790db92c0b3c9
jason-carbonai <jasondong1294@gmail.com>
2024-03-26 17:45:10 -0400

    logs

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,10 +8,12 @@ export async function GET(request: Request) {
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
+  console.log("CODE", code)
 
   if (code) {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
+    console.log("EXCHANGING")
     await supabase.auth.exchangeCodeForSession(code);
   }
 
