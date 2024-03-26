12657f59e2f52e47c69764f1bad2f790bdb0aa00
jason-carbonai <jasondong1294@gmail.com>
2024-03-26 17:55:56 -0400

    comment out redirect

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -18,5 +18,5 @@ export async function GET(request: Request) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin);
+  // return NextResponse.redirect(requestUrl.origin);
 }
