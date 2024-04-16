ee81a38b494fcdb522c7a437b0bc0038fbd73157
jason-carbonai <137559967+jason-carbonai@users.noreply.github.com>
2024-04-16 18:54:08 -0400

    fix: hide login page while waiting for redirect (#14)
    * style: paigo layout
    
    * documentation checkout token env
    
    * fix: use new token logic implemented by paigo for checkout
    
    * fix: check for eligibility for paigo after logging in
    
    * fix: refetch paigo eligibility
    
    * fix: hide login page while waiting for redirect
    
    ---------
    
    Co-authored-by: jason-carbonai <jasondong1294@gmail.com>

--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -56,7 +56,11 @@ const Checkout = (
     }, [planId])
 
     if (!paigoParams.toString()) {
-        return <Loader />
+        return (
+            <div className="mx-auto mt-10 flex w-full items-center justify-center">
+                <Loader />
+            </div>
+        );
     }
 
     console.log(paigoParams.toString())
